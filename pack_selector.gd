03c45f69a674f79716c5ffa7fb6a19a184950bc2
felipeljj <fmartins.2001@alunos.utfpr.edu.br>
2024-08-26 19:26:25 -0300

    name changes + visual changes

--- pack_selector.gd
+++ pack_selector.gd
@@ -3,21 +3,17 @@ extends Control
 var pack_state = {
 	"classico": true,
 	"nonsense": false,
-	"cuzinho": false
+	"weirdo": false
 }
-
 @onready var pack_panels = {
 	"classico": $MarginContainer/ScrollContainer/HBoxContainer/Classic,
 	"nonsense": $MarginContainer/ScrollContainer/HBoxContainer/NonSense,
-	"cuzinho": $MarginContainer/ScrollContainer/HBoxContainer/Cuzinho,
+	"weirdo": $MarginContainer/ScrollContainer/HBoxContainer/Weirdo,
 }
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	for pack_name in pack_state.keys():
 		update_pack_visual(pack_name)
 	
-
 func update_pack_visual(pack_name):
 	var panel = pack_panels[pack_name]
 	var is_active = pack_state[pack_name]
@@ -27,17 +23,14 @@ func _on_pack_panel_pressed(pack_name):
 	pack_state[pack_name] = !pack_state[pack_name]
 	update_pack_visual(pack_name)
 
-
 func _on_classic_pressed() -> void:
 	_on_pack_panel_pressed("classico")
 	
 func _on_nonsense_pressed() -> void:
 	_on_pack_panel_pressed("nonsense")
 	
-func _on_cuzinho_pressed() -> void:
-	_on_pack_panel_pressed("cuzinho")
+func _on_weirdo_pressed() -> void:
+	_on_pack_panel_pressed("weirdo")
 	
-
-
 func _process(delta: float) -> void:
 	pass
